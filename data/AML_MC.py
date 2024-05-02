cdaa02a80a2f14e9ca16c4152b9d87af51f597a1
alexfhnw <alexander.shanmugam@students.fhnw.ch>
2024-05-02 18:38:36 +0200

    EDA text added, Output format updated + SweetViz reports added

--- data/AML_MC.py
+++ data/AML_MC.py
@@ -1,7 +1,7 @@
-# %% [raw]
+# + active=""
 # title:  AML Challenge 2024
 # date: "Generated: {{ datetime.now().strftime('%Y-%m-%d') }}"
-# author: Etienne Roulet Alex Sha
+# author: Etienne Roulet, Alexander Shanmugam
 # output:
 #     general:
 #         input_jinja: true
@@ -9,23 +9,19 @@
 #         code_folding: hide
 #         code_tools: true
 #         theme: readable
+# -
 
-# %%
+# # Setup
+
+# %%capture
 # %pip install -r ../requirements.txt
 
-# %%
+# %%capture
 # %load_ext pretty_jupyter
 
-# %% [markdown]
-# ## Introduction
-#
-# In diesem Notebook wenden wir Applied Machine Learning (AML) Techniken an, um effektive Strategien für personalisierte Kreditkarten-Werbekampagnen zu entwickeln. Unser Ziel ist es, mithilfe von Kunden- und Transaktionsdaten präzise Modelle zu erstellen, die die Wahrscheinlichkeit des Kreditkartenkaufs vorhersagen.
-
-# %% [markdown]
-# ## Lib Importing
-#
-
-# %%
+# +
+# %%capture
+# Laden der eingesetzten Libraries
 import pandas as pd
 import os
 import numpy as np
@@ -38,15 +34,37 @@ from sklearn.preprocessing import StandardScaler
 from itables import init_notebook_mode
 from datetime import datetime
 from IPython.display import display
+import sweetviz as sv
 
 init_notebook_mode(all_interactive=True)
+# -
 
-# %% [markdown]
-# ## Load the Data
-#
+# # Aufgabenstellung
+# Inhalt der hier bearbeiteten und dokumentierten Mini-Challenge für das Modul «aml - Angewandtes Machine Learning» der FHNW ist die Entwicklung und Evaluierung von Aﬀinitätsmodellen für personalisierte Kreditkarten-Werbekampagnen im Auftrag einer Bank. Das Ziel der Authoren ist es also, mithilfe von Kunden- und Transaktionsdaten präzise Modelle zu erstellen, die die Wahrscheinlichkeit des Kreditkartenkaufs einer bestimmten Person vorhersagen.
+
+# +
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from itables import init_notebook_mode
+from datetime import datetime
+from IPython.display import display
+import sweetviz as sv
+
+init_notebook_mode(all_interactive=True)
+# -
+
+# ## Laden der zur Verfügung gestellten Daten
+# Zur Verfügung gestellt wurden 8 csv-Dateien von welchen die Beschreibung der erfassten Variablen unter dem folgenden Link eingesehen werden können: [PKDD'99 Discovery Challenge - Guide to the Financial Data Set](https://sorry.vse.cz/~berka/challenge/PAST/index.html). Nachfolgend werden diese csv-Dateien eingelesen.
 #
 
-# %%
+# +
 account = pd.read_csv("account.csv", sep=";", dtype={"date": "str"})
 account["date"] = pd.to_datetime(account["date"], format="%y%m%d")
 
@@ -66,71 +84,147 @@ order = pd.read_csv("order.csv", sep=";")
 
 trans = pd.read_csv("trans.csv", sep=";", dtype={"date": "str", "bank": "str"})
 trans["date"] = pd.to_datetime(trans["date"], format="%y%m%d")
-trans
-# count 'NaN' in each column from trans
+# -
 
-# %% [markdown]
-# ## EDA
+# # Explorative Datenanalyse
+# [//]: # (-.- .tabset)
+# In diesem Abschnitt wird mittels EDA ein Überblick über die eingelesenen Daten gewonnen. 
 
-# %% [markdown]
-# ### Account
+# ## account.csv
+# Der Datensatz `accounts.csv` beinhaltet die folgenden Informationen über die Kontos der Bank:  
+# - `account_id`: die Kontonummer, 
+# - `district_id`: den Standort der entsprechenden Bankfiliale,
+# - `issuance_statement_frequency`: die Frequenz der Ausstellung von Kontoauszügen (monatlich, wöchentlich, pro Transaktion) und 
+# - `date`: das Erstellungsdatum
 
-# %%
-account
+account.info()
 
-# %% [markdown]
-# ### Card
+# %%capture
+svReport_account = sv.analyze(account)
+svReport_account.show_html(filepath = "./reports/accounts.html", open_browser = False)
 
-# %%
-card
+# ## card.csv
+# Der Datensatz `card.csv` beinhaltet die folgenden Informationen über die von der Bank herausgegebenen Kreditkarten:  
+# - `card_id`: die Kartennummer, 
+# - `disp_id`: die Zuordnung zum entsprechenden Bankkonto und -inhaber (Disposition),
+# - `type`: die Art der Kreditkarte (junior, classic, gold) und 
+# - `issued`: das Ausstellungsdatum
 
-# %% [markdown]
-# ### Client
+card.info()
 
-# %%
-client
+# %%capture
+svReport_card = sv.analyze(card)
+svReport_card.show_html(filepath = "./reports/card.html", open_browser = False)
 
-# %% [markdown]
-# ### Disp
+# ## client.csv
+# Der Datensatz `client.csv` beinhaltet die folgenden Informationen über die Kunden der Bank:  
+# - `client_id`: die Kundennummer, 
+# - `birth_number`: eine Kombination aus Geburtsdatum und Geschlecht sowie
+# - `district_id`: die Adresse  
 
-# %%
-disp
+client.info()
 
-# %% [markdown]
-# ### District
+# %%capture
+svReport_client = sv.analyze(client)
+svReport_client.show_html(filepath = "./reports/client.html", open_browser = False)
 
-# %%
-district
+# ## disp.csv
+# Der Datensatz `disp.csv` beinhaltet die folgenden Informationen über die Dispositionen der Bank:  
+# - `disp_id`: der Identifikationsschlüssel der Disposition,
+# - `client_id`: die Kundennummer,
+# - `account_id`: die Kontonummer,
+# - `type`: die Art der Disposition (Inhaber, Benutzer)
 
-# %% [markdown]
-# ### Loan
+disp.info()
 
-# %%
-loan
+# %%capture
+svReport_disp = sv.analyze(disp)
+svReport_disp.show_html(filepath = "./reports/disp.html", open_browser = False)
 
-# %% [markdown]
-# ### Order
+# ## district.csv
+# Der Datensatz `district.csv` beinhaltet die folgenden demografischen Informationen:  
+# - `A1`: die ID des Distrikts, 
+# - `A2`: der Name des Distrikts,
+# - `A3`: die Region,
+# - `A4`: die Anzahl der Einwohner,
+# - `A5`: die Anzahl der Gemeinden mit < 499 Einwohner,
+# - `A6`: die Anzahl der Gemeinden mit 500 - 1999 Einwohner,
+# - `A7`: die Anzahl der Gemeinden mit 2000 - 9999 Einwohner,
+# - `A8`: die Anzahl der Gemeinden mit >10000 Einwohner,
+# - `A9`: die Anzahl Städte,
+# - `A10`: das Verhältnis von städtischen Einwohnern,
+# - `A11`: das durchschnittliche Einkommen,
+# - `A12`: die Arbeitslosenrate vom Jahr 95,
+# - `A13`: die Arbeitslosenrate vom Jahr 96,
+# - `A14`: die Anzahl von Unternehmer pro 1000 Einwohner,
+# - `A15`: die Anzahl von begangenen Verbrechen im Jahr 95,
+# - `A16`: die Anzahl von begangenen Verbrechen im Jahr 96,
 
-# %%
-order
+district.info()
 
-# %% [markdown]
-# ### Trans
+# %%capture
+svReport_district = sv.analyze(district)
+svReport_district.show_html(filepath = "./reports/district.html", open_browser = False)
 
-# %%
-trans
+# ## loan.csv
+# Der Datensatz `loan.csv` beinhaltet die folgenden Informationen über die vergebenen Darlehen der Bank:  
+# - `loan_id`: ID des Darlehens,
+# - `account_id`: die Kontonummer,
+# - `date`: das Datum, wann das Darlehen gewährt wurde,
+# - `amount`: der Betrag,
+# - `duration`: die Dauer des Darlehens,
+# - `payments`: die höhe der monatlichen Zahlungen und
+# - `status`: der Rückzahlungsstatus (A: ausgeglichen, B: Vertrag abgelaufen aber nicht fertig bezahlt, C: laufender Vertrag und alles in Ordnung, D: laufender Vertrag und Kunde verschuldet)
 
-# %% [markdown]
-# ## Transformations
+loan.info()
 
-# %%
+# %%capture
+svReport_loan = sv.analyze(loan)
+svReport_loan.show_html(filepath = "./reports/loan.html", open_browser = False)
+
+# ## order.csv
+# Der Datensatz `order.csv` beinhaltet die folgenden Informationen über die Daueraufträge eines Kontos:  
+# - `order_id`: die Nummer des Dauerauftrags,
+# - `account_id`: die Kontonummer von welchem der Auftrag stammt,
+# - `bank_to`: die empfangende Bank,
+# - `account_to`: das empfangende Konto, 
+# - `amount`: der Betrag,
+# - `k_symbol`: die Art des Auftrags (Versicherungszahlung, Haushalt, Leasing, Darlehen)
+
+order.info()
+
+# %%capture
+svReport_order = sv.analyze(order)
+svReport_order.show_html(filepath = "./reports/order.html", open_browser = False)
+
+# ## trans.csv
+# Der Datensatz `trans.csv` beinhaltet die folgenden Informationen über die Transaktionen eines Kontos:  
+# - `trans_id`: die ID der Transaktion,
+# - `account_id`: die Kontonummer des ausführenden Kontos,
+# - `date`: das Datum,
+# - `type`: der Typ (Einzahlung, Bezug)
+# - `operation`: die Art der Transaktion (Bezug Kreditkarte, Bareinzahlung, Bezug über eine andere Bank, Bezug Bar, Überweisung)
+# - `amount`: der Betrag der Transaktion,
+# - `balance`: der Kontostand nach ausführung der Transaktion,
+# - `k_symbol`: die Klassifikation der Transaktion (Versicherungszahlung, Kontoauszug, Zinsauszahlung, Zinszahlung bei negativem Kontostand, Haushalt, Pension, Darlehensauszahlung),
+# - `bank`: die empfangende Bank und 
+# - `account`: das empfangende Bankkonto
+
+trans.info()
+
+# %%capture
+svReport_trans = sv.analyze(trans)
+svReport_trans.show_html(filepath = "./reports/trans.html", open_browser = False)
+
+# # Transformations
+# [//]: # (-.- .tabset)
+
 data_frames = {}
 
-# %% [markdown]
-# ### Account
+# ## Account
 #
 
-# %%
+# +
 # Frequency Transformation
 account["frequency"] = account["frequency"].replace(
     {
@@ -151,18 +245,17 @@ data_frames["account.csv"] = account
 
 # Sample 5 random rows
 account.sample(n=5)
+# -
 
-# %% [markdown]
-# ### Card
+# ## Card
 
-# %%
 card["issued"] = pd.to_datetime(card["issued"], format="mixed")
 data_frames["card.csv"] = card
 
-# %% [markdown]
-# ### Client
 
-# %%
+# ## Client
+
+# +
 # Funktion zur Bestimmung des Geschlechts und Berechnung des Geburtstags
 def parse_details(birth_number):
     birth_number_str = str(
@@ -199,21 +292,20 @@ data_frames["client.csv"] = client
 # Auswahl spezifischer Spalten für die finale DataFrame (optional, je nach Bedarf)
 # Sample 5 random rows
 client.sample(n=5)
+# -
 
-# %% [markdown]
-# ### Disp
+# ## Disp
 
-# %%
+# +
 data_frames["disp.csv"] = disp
 
 # random sample
 disp.sample(n=5)
+# -
 
-# %% [markdown]
-# ### District
+# ## District
 #
 
-# %% [markdown]
 # - A1 district_id/district code
 # - A2 district name
 # - A3 region
@@ -230,7 +322,7 @@ disp.sample(n=5)
 # - A15 no. of commited crimes ’95
 # - A16 no. of commited crimes ’96
 
-# %%
+# +
 import pandas as pd
 
 # Assuming 'district' is your pandas DataFrame
@@ -280,16 +372,14 @@ data_frames["district.csv"] = district
 
 district.sample(n=5)
 district
+# -
 
-# %%
 # find the ? in the district dataframe
 district[district.isin(["?"]).any(axis=1)]
 
-# %%
 # replace the ? with NaN
 district = district.replace("?", np.nan)
 
-# %%
 # replace the NaN with the mean of the column no_of_crimes95 and unemploy_rate95
 district["no_of_crimes95"] = district["no_of_crimes95"].astype(float)
 district["unemploy_rate95"] = district["unemploy_rate95"].astype(float)
@@ -302,10 +392,9 @@ district["unemploy_rate95"] = district["unemploy_rate95"].fillna(
 # check if there are still NaN values in no_of_crimes95 and unemploy_rate95
 district[district.isin([np.nan]).any(axis=1)]
 
-# %% [markdown]
-# ### Loan
+# ## Loan
 
-# %%
+# +
 # Convert the 'date' column to datetime format
 loan["date"] = pd.to_datetime(loan["date"], format="mixed")
 
@@ -330,7 +419,7 @@ num_of_loan_df = (
 # Display the resulting DataFrame
 num_of_loan_df
 
-# %%
+# +
 # Perform an inner join between 'loan' and 'num_of_loan_df' on 'account_id'
 loan = pd.merge(loan, num_of_loan_df, on="account_id", how="inner")
 
@@ -339,15 +428,14 @@ data_frames["loan.csv"] = loan
 
 # Sample 5 random rows from the joined DataFrame
 loan.sample(n=100)
+# -
 
-# %% [markdown]
-# ### Order
+# ## Order
 #
 
-# %%
 order
 
-# %%
+# +
 # Assuming 'order' and 'account' DataFrames are already loaded
 
 # Correctly map and fill missing values in 'k_symbol' column
@@ -388,15 +476,14 @@ data_frames["order.csv"] = orders_pivot
 data_frames["order.csv"] = data_frames["order.csv"].fillna(0)
 # Sample 5 random rows from the merged DataFrame
 data_frames["order.csv"].sample(n=10)
+# -
 
-# %%
 data_frames["order.csv"].columns
 
-# %% [markdown]
-# ### Trans
+# ## Trans
 #
 
-# %%
+# +
 # Convert 'date' from string to datetime
 trans["date"] = pd.to_datetime(trans["date"])
 
@@ -427,6 +514,9 @@ trans["k_symbol"] = trans["k_symbol"].replace(
     }
 )
 
+# negate the amount if type is credit
+trans.loc[trans['type'] == 'credit', 'amount'] = trans.loc[trans['type'] == 'credit', 'amount'] * (-1)
+
 # Assign to a dictionary if needed (similar to list assignment in R)
 
 data_frames["trans.csv"] = trans
@@ -435,7 +525,7 @@ data_frames["trans.csv"] = trans
 trans.sample(n=1000)
 trans
 
-# %%
+# +
 # Plot Zeitliche Entwicklung des Konto-Saldos für die Konto nummer 19
 account_19 = trans[trans["account_id"] == 19].copy()  # Create a copy of the DataFrame
 # Ensure the date column is in datetime format
@@ -450,8 +540,8 @@ plt.title("Time evolution of balance for account number 19")
 plt.xlabel("Date")
 plt.ylabel("Balance")
 plt.show()
+# -
 
-# %%
 # zoom the year 1995 of the plot
 account_19_1995 = account_19[account_19["date"].dt.year == 1995]
 # plot it
@@ -462,23 +552,18 @@ plt.xlabel("Date")
 plt.ylabel("Balance")
 plt.show()
 
-# %% [markdown]
 # Wee see that there is a steep line in 1995-10 so there are two transactions, this we have to clean.
 
-# %% [markdown]
-# ## D&Q
+# # D&Q
 
-# %%
 # Check for missing values in each DataFrame
 for df_name, df in data_frames.items():
     print(f"Missing values in {df_name}:")
     print(df.isna().sum().sum())  # Sum of all missing values in the DataFrame
 
+# # Merge the Dataframe['XXX'] for non transaction Data
 
-# %% [markdown]
-# ## Merge the Dataframe['XXX'] for non transaction Data
-
-# %%
+# +
 # merge dataframes
 
 
@@ -510,12 +595,11 @@ non_transactional_data = (
         how="left",
     )
 )
+# -
 
-
-# %%
 non_transactional_data.columns
 
-# %% With the merge we got NaN values in the columns, we have to clean them
+# +
 cols_to_replace_na = [
     "household_order",
     "insurance_payment_order",
@@ -527,9 +611,8 @@ cols_to_replace_na = [
 non_transactional_data[cols_to_replace_na] = non_transactional_data[
     cols_to_replace_na
 ].fillna(0)
+# -
 
-
-# %%
 # ## Dropping of Junior Cards that are not on the edge to a normal card Analyse
 #
 # join disctrict and client left join on district_id
@@ -540,10 +623,8 @@ non_transactional_data = non_transactional_data.merge(
     how="left",
 )
 
-# %%
 non_transactional_data
 
-# %%
 # merge client with suffix
 non_transactional_data = non_transactional_data.merge(
     data_frames["client.csv"].add_suffix("_client"),
@@ -552,8 +633,7 @@ non_transactional_data = non_transactional_data.merge(
     how="left",
 )
 
-
-# %%
+# +
 non_transactional_data["has_card"] = ~non_transactional_data["card_id_card"].isna()
 
 # Filter rows where 'has_card' is True
@@ -565,11 +645,11 @@ duplicated_account_id = filtered_data["account_id_account"].duplicated().sum()
 print(duplicated_account_id)
 
 
+# -
 
-# %% [markdown]
-# ### Junior Cards removal
+# ## Junior Cards removal
 
-# %%
+# +
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -597,12 +677,10 @@ plt.title("Age distribution at issue date of junior cards")
 plt.xlabel("Age at issue date")
 plt.ylabel("Number of cards")
 plt.show()
+# -
 
-
-# %% [markdown]
 # In the advertising campaign, we do not want to promote children's/junior cards (for whatever reasons). First, I looked at the distribution of age at issuance. Here I see that there are not many junior cards, nor are the cards issued at a late age.
 
-# %%
 num_accounts_before = len(non_transactional_data)
 # Filter rows where 'card_type' does not contain 'junior' (case insensitive)
 non_transactional_data = non_transactional_data[
@@ -612,43 +690,38 @@ num_accounts_after = len(non_transactional_data)
 num_junior_cards = num_accounts_before - num_accounts_after
 print(f"Number of junior cards removed: {num_junior_cards}")
 
-
-# %% [markdown]
 #
 
-# %% [markdown]
-# ## Convert the Notebook always to a py file and vis versa
+# # Convert the Notebook always to a py file and vice versa
 
-# %%
-import subprocess
-import pathlib
+# +
+# import subprocess
+# import pathlib
 
 
-try:
-    file_path = pathlib.Path(os.path.basename(__file__))
-except:
-    file_path = pathlib.Path("AML_MC.ipynb")
+# try:
+#     file_path = pathlib.Path(os.path.basename(__file__))
+# except:
+#     file_path = pathlib.Path("AML_MC.ipynb")
 
 # Check the file extension
-if file_path.suffix == ".py":
-    # If it's a Python script, convert it to a notebook
-    try:
-        subprocess.check_output(["jupytext", "--to", "notebook", str(file_path)])
-        print("Converted to notebook.")
-    except subprocess.CalledProcessError as e:
-        print("Conversion failed. Error message:", e.output)
-elif file_path.suffix == ".ipynb":
-    # If it's a notebook, convert it to a Python script with cell markers
-    try:
-        subprocess.check_output(["jupytext", "--to", "py:percent", str(file_path)])
-        print("Converted to Python script.")
-    except subprocess.CalledProcessError as e:
-        print("Conversion failed. Error message:", e.output)
-else:
-    print("Unsupported file type.")
+# if file_path.suffix == ".py":
+#     # If it's a Python script, convert it to a notebook
+#    try:
+#         subprocess.check_output(["jupytext", "--to", "notebook", str(file_path)])
+#         print("Converted to notebook.")
+#     except subprocess.CalledProcessError as e:
+#         print("Conversion failed. Error message:", e.output)
+# elif file_path.suffix == ".ipynb":
+#     # If it's a notebook, convert it to a Python script with cell markers
+#     try:
+#         subprocess.check_output(["jupytext", "--to", "py:percent", str(file_path)])
+#         print("Converted to Python script.")
+#     except subprocess.CalledProcessError as e:
+#         print("Conversion failed. Error message:", e.output)
+# else:
+#     print("Unsupported file type.")
 
-# %%
+# +
 # command with os
-os.system("jupyter nbconvert --to html --template pj AML_MC.ipynb")
-
-
+# os.system("jupyter nbconvert --to html --template pj AML_MC.ipynb")
